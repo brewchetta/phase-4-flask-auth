546d8bb4cded6832bccce874f04c7923167cbe92
brewchetta <chett.tiller@gmail.com>
2023-06-27 10:18:59 -0400

    changed secret key for ease of testing

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ from flask_migrate import Migrate
 from models import db, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.secret_key = 'iamasecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
