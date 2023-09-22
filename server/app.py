a0a5d5f6cbd4a9022ba96c59dbd223c7df737c12
brewchetta <chett.tiller@gmail.com>
2023-09-22 11:36:58 -0400

    major updates

--- server/app.py
+++ server/app.py
@@ -6,10 +6,10 @@
 # bcrypt.generate_password_hash(password).decode('utf-8')
 # bcrypt.check_password_hash(hashed_password, password)
 
-from flask import Flask, request, session
+from flask import Flask, jsonify, request, session
 from flask_migrate import Migrate
 
-from models import db, User
+from models import db, User, Note
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -21,36 +21,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+URL_PREFIX = '/api/v1'
+
+# HELPER METHODS #
+
+# something will go here later
 
 # USER SIGNUP #
 
-@app.post('/api/users')
+@app.post('/users')
 def create_user():
-    json = request.json
-    new_user = User(username=json['username'])
-    db.session.add(new_user)
-    db.session.commit()
-    return new_user.to_dict(), 201
-
+    try:
+        json = request.json
+        new_user = User(username=json['username'])
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user.to_dict(), 201
+    except Exception as e:
+        return { 'error': str(e) }, 406
 
 
 # SESSION LOGIN/LOGOUT#
 
-@app.post('/api/login')
+@app.post(URL_PREFIX + '/login')
 def login():
     pass
 
 
-# EXAMPLE OTHER RESOURCES WITH AUTH #
+# EXAMPLE OTHER RESOURCES #
 
-@app.get('/api/cartoons')
-def get_cartoons():
-    return [
-        { 'id': 1, 'name': 'Teenage Mutant Ninja Turtles' },
-        { 'id': 2, 'name': 'Powerpuff Girls' },
-        { 'id': 3, 'name': 'Thunder Cats' }
-    ], 200
+@app.get(URL_PREFIX + '/notes')
+def get_notes():
+    return jsonify( [note.to_dict() for note in Note.query.all()] ), 200
 
+@app.post(URL_PREFIX + '/notes')
+def create_note():
+    try:
+        data = request.json
+        new_note = Note(**data)
+        db.session.add(new_note)
+        db.session.commit()
+        return jsonify( new_note.to_dict() ), 201
+    except Exception as e:
+        return jsonify( {'error': str(e)} ), 406
 
 # APP RUN #
 
--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -7,17 +9,33 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class User(db.Model):
+# --- USER --- #
+
+class User(db.Model, SerializerMixin):
+    # TABLE #
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
+    username = db.Column(db.String, unique=True, nullable=False)
 
-    def __repr__(self):
-        return f'<User id="{self.id}" username="{self.username}">'
+    # RELATIONSHIP #
+    notes = db.relationship('Note', back_populates='user')
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "username": self.username
-        }
+    # SERIALIZER #
+    serialize_rules = ("-notes",)
+
+
+# --- NOTE --- #
+
+class Note(db.Model, SerializerMixin):
+    # TABLE #
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+
+    # RELATIONSHIP #
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # SERIALIZER #
+    user = db.relationship('User', back_populates='notes')
+
+    serialize_rules = ("-user",)
